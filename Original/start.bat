0c713081e9dcfe759f459530dd0e633def32ac43
rajat dangwal <rajatdangwal0@gmail.com>
2019-02-12 22:18:57 +0530

    Update start.bat

--- Original/start.bat
+++ Original/start.bat
@@ -1,10 +1,12 @@
 ECHO OFF
 ECHO It will take only few minutes
 npm install
+npm install bootstrap
+npm install jquery
 npm install ejs
 npm install express
 npm install multer
-
+npm i -S formidable
 
 
  
